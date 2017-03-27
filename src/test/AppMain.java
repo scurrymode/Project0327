7bafe2259f2122862ea069b48a9db8eeccb91251
DESKTOP-I8P6V4B\user <impactorz@gmail.com>
2017-03-27 10:01:13 -0700

    Ver 1.1

--- src/test/AppMain.java
+++ src/test/AppMain.java
@@ -3,8 +3,11 @@ package test;
 public class AppMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=5;
+		System.out.println((char)a);
+		
+		System.out.println((short)a);
+		
 	}
 
 }
